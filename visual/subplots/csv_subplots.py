3852e2d911ff85703781ac179e3f2f9d1b59388d
Hayri Zelhar <5DEYYH32@solent.ac.uk>
2020-11-24 10:27:52 +0000

    "Added code to display a simple animation."

---
+++ visual/subplots/csv_subplots.py
@@ -0,0 +1,39 @@
+import csv
+import matplotlib.pyplot as plt
+
+first_key = None
+second_key = None
+
+def read_data():
+  global first_key, second_key
+  with open('visual/subplots/temps.csv') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    header = next(csv_reader)
+
+    first_key = header[0].strip()
+    second_key = header[1].strip()
+
+    data = {
+      first_key:[],
+      second_key:[]
+    }
+
+    for row in csv_reader:
+      data[first_key].append(row[0].strip())
+      data[second_key].append(row[1].strip())
+
+    return data
+
+
+def run():
+  data = read_data()
+
+  fig, (ax1, ax2) = plt.subplots(2, 1, sharex='all')
+  #going with (ax1, ax2), because they're only two
+  #there is other ways that this can be done
+  ax1.plot(data[first_key])
+  ax2.plot(data[second_key])
+  plt.show()
+
+run()
+
